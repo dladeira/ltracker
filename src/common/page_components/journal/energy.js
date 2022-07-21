aa2873da1cb4a62c9fa95f90c24cfac8b36aec34
dladeira <dladeiratx@gmail.com>
2022-07-21 12:57:23 +0200

    Fixed energy levels being in reverse

--- src/common/page_components/journal/energy.js
+++ src/common/page_components/journal/energy.js
@@ -50,7 +50,7 @@ function Canvas() {
     }
 
     const hours = ["6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM"]
-    const percents = ["0%", "", "20%", "", "40%", "", "60%", "", "80%", "", "100%"]
+    const percents = ["100%", "", "80%", "", "60%", "", "40%", "", "20%", "", "0%"]
     const pointSize = 15
 
     useEffect(() => {
